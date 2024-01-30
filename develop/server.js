89ac23b75992840d080275213770a56d092b3546
mmorrow6109 <msmorrow6109@gmail.com>
2024-01-29 19:05:44 -0800

    .env implemented.  values obscured

--- develop/server.js
+++ develop/server.js
@@ -1,13 +1,19 @@
 // Import required modules
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+require('dotenv').config();
 
 // Create MySQL connection
 const connection = mysql.createConnection({
   host: 'localhost',
-  port: 3001,
-  user: 'mmorrow6109',
-  password: 'mySQLTweezer6109!',
-  database: 'employees_db'
+  port: 3001, 
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
+// Connect to the MySQL server and SQL database
+connection.connect(function(err) {
+  if (err) throw err;
+  // Start your server or run your functions here
+});
